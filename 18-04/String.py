0bd2571c277e8370af2391f3cb1aed20af501def
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-18 08:50:36 -0300

    aula 18-04

---
+++ 18-04/String.py
@@ -0,0 +1,15 @@
+# st = "Hello"
+# print(st[0])
+# print(st[2:5])
+
+# Strings sao imuyaveis. 
+# st = "don't worry, be happy"
+# outra_st = st[4:8]
+# tam = len(outra_st)
+# print (outra_st)
+# print (tam)
+ 
+# print (outra_st.upper())
+
+for x in range (1,100):
+    print(x)
